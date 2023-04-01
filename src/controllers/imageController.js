82e7fd8c395dc9c7207d3b8121c4cf1ff71546e6
FabioKido <fabiohenryquemesquita@gmail.com>
2023-04-01 13:08:29 -0300

    feat: preview image

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -1,6 +1,6 @@
 const renderSocialImage = require('puppeteer-social-image')
 
-exports.show = async (req, res) => {
+exports.store = async (req, res) => {
 
     try {
         const { title, size } = req.query;
@@ -12,15 +12,18 @@ exports.show = async (req, res) => {
                     "https://images.unsplash.com/photo-1557958114-3d2440207108?w=1950&q=80",
                 title
             },
-            output: "assets/image.png",
+            output: "images/image.png",
             size
         })
 
         res.status(200).json({
             message: 'image created with success'
         });
-
     } catch (error) {
         // error message
     }
+}
+
+exports.show = async (req, res) => {
+    res.send('<img src="/images/image.png" alt="">')
 }
--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -4,6 +4,7 @@ const imageController = require('../controllers/imageController');
 
 const imageRouter = Router();
 
-imageRouter.get('/new', imageController.show);
+imageRouter.get('/new', imageController.store);
+imageRouter.get('/preview', imageController.show);
 
 module.exports = imageRouter
